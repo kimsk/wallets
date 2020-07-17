a634ddab10ad2bb50bf95eec552bd528d5c51838
Richard Kiss <him@richardkiss.com>
2020-07-16 17:46:36 -0700

    Improve test code.

--- coloured_coins/test_cc.py
+++ coloured_coins/test_cc.py
@@ -28,6 +28,9 @@ KFA = {
 }
 
 
+CONDITIONS = {v: k for k, v in KFA.items()}
+
+
 def sha256(args):
     h = hashlib.sha256()
     for _ in args.as_iter():
@@ -36,13 +39,15 @@ def sha256(args):
     return h.digest()
 
 
-def sha256tree(v):
+def sha256tree(v, literals=[]):
     if v.listp():
-        left = sha256tree(v.first())
-        right = sha256tree(v.rest())
+        left = sha256tree(v.first(), literals)
+        right = sha256tree(v.rest(), literals)
         s = b"\2" + left + right
     else:
         atom = v.as_atom()
+        if atom in literals:
+            return atom
         s = b"\1" + atom
     return hashlib.sha256(s).digest()
 
@@ -63,8 +68,77 @@ def load_clvm(path, search_paths):
     return s
 
 
-def cc_puzzle_for_inner_puzzle(mod_code, genesis, inner_puzzle):
-    return curry(mod_code, [sha256tree(mod_code), genesis, inner_puzzle])
+def cc_puzzle_for_inner_puzzle(mod_code, genesis_coin_hash, inner_puzzle):
+    return curry(mod_code, [sha256tree(mod_code), genesis_coin_hash, inner_puzzle])
+
+
+def cc_ph_for_inner_ph(mod_code, genesis_coin_hash, inner_ph):
+    return sha256tree(
+        curry(mod_code, [sha256tree(mod_code), genesis_coin_hash, inner_ph]), [inner_ph]
+    )
+
+
+def lock_ph_for_inputs(mod_code, genesis_coin_hash, input_coins):
+    return b"LOCK_VALUE"
+
+
+def solution_for_inputs(
+    mod_code,
+    genesis_coin_hash,
+    input_coins,
+    input_index,
+    output_iph_amount_pairs,
+    iph_for_cc_ph_f,
+    coin_for_id_f,
+):
+    # returns condition list, [parent-proofs, input-proofs, output-proofs]
+    conditions = []
+    input_coin = input_coins[input_index]
+    input_coin_id = sha256(to_sexp_f(input_coin))
+
+    #
+    conditions.append([CONDITIONS["ASSERT_MY_COIN_ID"], input_coin_id])
+
+    # assert lock coin consumed
+    lock_ph = lock_ph_for_inputs(mod_code, genesis_coin_hash, input_coins)
+    lock_coin = [input_coin_id, lock_ph, 0]
+    lock_coin_id = sha256(to_sexp_f(lock_coin))
+    # conditions.append([CONDITIONS["ASSERT_COIN_CONSUMED"], lock_coin_id])
+
+    # create lock coin
+    conditions.append([CONDITIONS["CREATE_COIN"], lock_ph, 0])
+
+    proofs = []
+    parent_proofs = []
+    proofs.append(parent_proofs)
+
+    input_proofs = []
+    proofs.append(input_proofs)
+    output_proofs = [0]
+    proofs.append(output_proofs)
+
+    if input_index == 0:
+        # build proofs and additions conditions
+
+        for input_coin in input_coins:
+            input_ph = input_coin[1]
+            input_iph = iph_for_cc_ph_f(input_ph)
+            input_proofs.append(input_iph)
+
+            parent_coin = coin_for_id_f(input_coin[0])
+            if parent_coin:
+                parent_ph = parent_coin[1]
+                parent_iph = iph_for_cc_ph_f(parent_ph)
+                parent_proofs.append(parent_iph)
+            else:
+                parent_proofs.append(0)
+
+        for ph, amount in output_iph_amount_pairs:
+            output_ph = cc_ph_for_inner_ph(mod_code, genesis_coin_hash, ph)
+            condition = [CONDITIONS["CREATE_COIN"], output_ph, amount]
+            conditions.append(condition)
+            output_proofs.append(ph)
+    return conditions, proofs
 
 
 def main():
@@ -78,17 +152,16 @@ def main():
     genesis_puzzle_hash = assemble("1")  # return the solution verbatim
     genesis_coin = [b"GENESIS", genesis_puzzle_hash, 100000]
 
-    mod_code_hash = to_sexp_f(sha256tree(mod_code))
     prog = mod_code
     genesis_coin_hash = sha256(to_sexp_f(genesis_coin))
 
     inner_puzzle = assemble("1")
     inner_puzzle_hash = sha256tree(inner_puzzle)
 
-    list_of_input_coins = [
+    input_coins = [
         [genesis_coin_hash, inner_puzzle_hash, 500],
         [genesis_coin_hash, inner_puzzle_hash, 1500],
-        [genesis_coin_hash, inner_puzzle_hash, 2500],
+        [genesis_coin_hash, inner_puzzle_hash, 2501],
     ]
 
     cc_puzzle = cc_puzzle_for_inner_puzzle(mod_code, genesis_coin_hash, inner_puzzle)
@@ -97,24 +170,51 @@ def main():
 
     prog = cc_puzzle
 
+    dest_inner_puzzle = to_sexp_f(1)
+    dest_inner_ph = sha256tree(dest_inner_puzzle)
+
+    dest_inner_ph_2 = sha256tree(to_sexp_f(2))
+    dest_puzzle = cc_puzzle_for_inner_puzzle(
+        mod_code, genesis_coin_hash, dest_inner_puzzle
+    )
+    dest_ph = sha256tree(dest_puzzle)
+
+    output_iph_amount_pairs = [
+        [dest_inner_ph, 4500],
+        [dest_inner_ph_2, 1],
+    ]
+
+    COINS = []
+    coins_dict = dict((sha256(to_sexp_f(_)), _) for _ in COINS)
+
+    IPH = [inner_puzzle_hash]
+    ph_to_iph_dict = dict(
+        (cc_ph_for_inner_ph(mod_code, genesis_coin_hash, _), _) for _ in IPH
+    )
+
     for input_index in range(3):
-        my_id = sha256(to_sexp_f(list_of_input_coins[input_index]))
-        dest_inner_puzzle = to_sexp_f(1)
-        dest_puzzle = cc_puzzle_for_inner_puzzle(mod_code, genesis_coin_hash, dest_inner_puzzle)
-        dest_ph = sha256tree(dest_puzzle)
-        coin_value = 4500
-        if input_index == 0:
-            inner_puzzle_solution = assemble(f"((51 LOCK_VALUE 0) (53 0x{my_id.hex()}) (51 0x{dest_ph.hex()} {coin_value}))")
-        else:
-            inner_puzzle_solution = assemble(f"((51 LOCK_VALUE 0) (53 0x{my_id.hex()}))")
-        solution = to_sexp_f([list_of_input_coins, inner_puzzle_solution, input_index, [0, 0, 0], [0, 0, 0], [0, sha256tree(dest_inner_puzzle)]])
+        inner_puzzle_solution, proofs = solution_for_inputs(
+            mod_code,
+            genesis_coin_hash,
+            input_coins,
+            input_index,
+            output_iph_amount_pairs,
+            ph_to_iph_dict.get,
+            coins_dict.get,
+        )
+
+        my_id = sha256(to_sexp_f(input_coins[input_index]))
+
+        solution = [input_coins, inner_puzzle_solution, input_index]
+        solution.extend(proofs)
+        solution = to_sexp_f(solution)
         args = solution
         print(f"\nbrun -y main.sym '{bu_disassemble(prog)}' '{bu_disassemble(args)}'")
         cost, r = run_program(prog, args)
         print(disassemble(r))
 
     for _ in range(0):
-        parent_coin_id = sha256(to_sexp_f(list_of_input_coins[_]))
+        parent_coin_id = sha256(to_sexp_f(input_coins[_]))
         my_coin_id = sha256(to_sexp_f([parent_coin_id, cc_puzzle_hash, 0]))
         print(f"my_coin_id[{_}] = {my_coin_id.hex()}")
 
